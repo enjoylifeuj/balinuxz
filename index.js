95f3202d6f5047cbd18deb0f4350ae407e06aee3
enjoylifeuj <a495403288@sina.com>
2021-01-05 05:42:46 +0800

    update: joke and update json

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-console.log(' Run Action 1');
-console.log(' Run Action 2');
-console.log(' Run Action 3');
-console.log(' Run Action 4');
-console.log(' Run Action 5');
+const fs = require('fs');
+const axios = require('axios');
+
+/**
+ * get data joke
+ */
+
+function getJoke() {
+    axios.get('https://api.chucknorris.io/jokes/random')
+        .then((data) => {
+            console.log(data.data);
+            updateFile(data.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
+
+/**
+ * update file
+ */
+
+function updateFile(joke) {
+    fs.readFile('./data/joke.json', 'utf8', function(error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            const file = JSON.parse(data);
+            file.randomJoke.push(joke);
+            const toStringData = JSON.stringify(file);
+
+            fs.writeFile('./data/joke.json', toStringData, 'utf8', function(error) {
+                if (error) {
+                    console.log(error)
+                } else {
+                    console.log("Success update data");
+                }
+            })
+        }
+    })
+}
+getJoke();
